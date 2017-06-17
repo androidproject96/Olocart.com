a1c6abae790a60ae5f4174419d10cd9b1e27c2b7
aastha <aasthagoel96@gmail.com>
2017-06-17 17:07:37 +0530

    collapsing toolbar

--- app/src/main/java/com/project/olocartcom/MainActivity.java
+++ app/src/main/java/com/project/olocartcom/MainActivity.java
@@ -1,13 +1,33 @@
 package com.project.olocartcom;
 
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+import android.support.design.widget.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout=(CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Home");
+        collapsingToolbarLayout.setContentScrimColor(Color.GREEN);
+        getStarted();
+
     }
+   public void getStarted()
+    {
+
+    }
+
 }
